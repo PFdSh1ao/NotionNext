838f8733330a7bc58e76fbd6f582bcddc073df62
Sh1ao_O <100872127+PFdSh1ao@users.noreply.github.com>
2024-10-20 15:56:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '寻者逍Sh1ao_O', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '中传游戏设计系本科在读\n欢迎来到我的个人网站\n该网站正在建设，不代表最终效果（还有好多好多文章没传TAT）', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '中传游戏设计系本科在读\n欢迎来到我的个人网站\n该网站正在建设，不代表最终效果', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
